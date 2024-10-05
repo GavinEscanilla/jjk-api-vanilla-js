66551836ab006af1a7e2521f553e07fc48868a1d
GavinEscanilla <gavinaltonescanilla@gmail.com>
2024-10-05 01:56:34 -0400

    added an animation and hover effect with audiomplaying

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const root = document.getElementById('root');
-
+const container = document.getElementById('container');
       const jjk =[
         {
             name: 'yuji',
@@ -73,17 +73,43 @@ const root = document.getElementById('root');
             power: 'Mythical East Amber',
             nickname: 'Strongest of the Edo peroid'
         }
+
       ];
-      const details= jjk.forEach((item) => {
+      jjk.forEach((item) => {
         const div = document.createElement('div');
-        div.id = `jjk-${item.name}`;
+        div.id = `${item.name}`;
         div.classList.add('jjk-info');
         div.innerHTML = `
           <h2>Name:  ${item.name}</h2>
           <p>Age: ${item.age}</p>
           <p>Cursed Technique: ${item.power}</p>
         `;
-        root.appendChild(div);
+        container.appendChild(div);
       });
-        
-      console.log('hello');
+const nobara = document.getElementById('nobara');
+const nobara_audio = new Audio('audio/you-drop-even-one-of-those-bags-and-ill-kill-you-101soundboards.mp3');
+     nobara.addEventListener('mouseenter', () => {
+        nobara_audio.play();
+     });
+     nobara.addEventListener('mouseleave', () => {
+        nobara_audio.pause();
+        nobara_audio.currentTime = 0;
+     });
+const gojo = document.getElementById('gojo');
+const gojo_audio = new Audio('audio/Voicy_gojo saturo .mp3');
+     gojo.addEventListener('mouseenter', () => {
+        gojo_audio.play();
+     });
+     gojo.addEventListener('mouseleave', () => {
+        gojo_audio.pause();
+        gojo_audio.currentTime = 0;
+     });
+     const yuta = document.getElementById('yuta');
+     const yuta_audio = new Audio('audio/it’s-pure-love-(yuta-and-rika-vs-geto)-made-with-Voicemod.mp3');
+     yuta.addEventListener('mouseenter', () => {
+        yuta_audio.play();
+     });
+     yuta.addEventListener('mouseleave', () => {
+        yuta_audio.pause();
+        yuta_audio.currentTime = 0;
+     });
